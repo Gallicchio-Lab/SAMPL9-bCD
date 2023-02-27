a559bc93094c41229ab1f658a3071c55ea516703
sheenam1509 <sheenam.1509@gmail.com>
2023-02-27 16:43:03 -0500

    metaD input files

---
+++ RBFE-metaD/mCD/mCDssS-PMZ1assS-PMT1assS/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#!/bin/bash
+#SBATCH -J mCD-ssS
+#SBATCH --partition=gpu-shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus=1
+#SBATCH --account=TG-MCB150001
+#SBATCH --no-requeue
+#SBATCH -t 26:00:00
+
+module purge
+module load gpu
+module load cuda10.2/blas/10.2.89 
+
+echo "Number of nodes: $SLURM_NNODES"
+echo "Nodelist: $SLURM_NODELIST"
+echo "Number of tasks: $SLURM_NTASKS"
+echo "Tasks per node: $SLURM_TASKS_PER_NODE"
+echo "Assigned GPU: $CUDA_VISIBLE_DEVICES"
+
+/home/u15684/software/openmm-7.6.0-ATMMetaForce-0.3.0-CUDA-10.2-Python-3.7/runopenmm /expanse/lustre/projects/rut147/sheenam/async_re-openmm/rbfe_explicit.py mCDssS-PMZ1assS-PMT1assS_asyncre.cntl
